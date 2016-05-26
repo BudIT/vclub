a2d012bf98b587b83819de192db6b83e8d8d3a22
Ruslan <frodohrucik@gmail.com>
2016-05-26 18:00:16 +0300

    initial setup express server

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React                   from 'react';
+import { Route, IndexRoute }   from 'react-router';
+import ClubLayoutRoot from '../src/views/clubLayout/Container';
+
+export default (
+  <Route name="app" component={ClubLayoutRoot} path="/">
+  </Route>
+);
