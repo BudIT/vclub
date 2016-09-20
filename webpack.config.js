3e9f4f20e79271f85979af4d60e1c2358e3e0e50
Yanis <yanis.urbis@gmail.com>
2016-09-20 17:50:06 +0300

    change webpack config, babel config and index

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,52 @@
-var path = require('path');
+//  __dirname = https://nodejs.org/docs/latest/api/globals.html#globals_dirname
+
 var webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    // site: path.resolve(__dirname, 'src/site.js'),
-    club: path.resolve(__dirname, 'src/club.js'),
-  },
+  entry: [
+    './src/club.js'
+  ],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].bundle.js',
+    path: __dirname,
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
+      // enabling the latest JS abilities
+      // check .babelrc
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css/,
+        loaders: ['style', 'css'],
+      }
     ],
   },
   resolve: {
     alias: {
-      vclub: path.resolve(__dirname, 'src'),
+      // can
+        // import Component from 'vclub/component/Component'
+      // even we don't have vclub directory or module
+      vclub: __dirname + '/src',
     },
   },
   plugins: [
+    // read https://webpack.github.io/docs/list-of-plugins.html#defineplugin
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
       },
     }),
   ],
+  devServer: {
+    // automatic refresh on change
+    inline: true,
+    // server port
+    port:3333,
+  },
+  // easy to debug
+  devtool: "source-map"
 };
