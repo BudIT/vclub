c6292adf819c200370888078f3d62b3d8a0b5e59
Yanis <yanis.urbis@gmail.com>
2016-09-30 12:55:06 +0300

    add small change in layout rendering

--- src/views/clubLayout/ClubLayout.js
+++ src/views/clubLayout/ClubLayout.js
@@ -3,9 +3,7 @@ import React from 'react';
 import compose from 'recompose/compose';
 import { connect } from 'react-redux';
 
-import NavLink from 'vclub/components/NavLink/NavLink';
-import Nav from 'vclub/components/Nav/Nav';
-
+import Header from '../header/Header';
 
 const enhance = compose(
   connect(state => ({ state })),
@@ -14,15 +12,7 @@ const enhance = compose(
 function ClubLayout() {
   return (
     <div>
-      <header>
-        <Nav>
-          <NavLink>Sharing</NavLink>
-          <NavLink>Video</NavLink>
-          <NavLink>Chat</NavLink>
-          <NavLink>Whiteboard</NavLink>
-          <NavLink>Menu</NavLink>
-        </Nav>
-      </header>
+      <Header />
       <main>
         Content will be there
       </main>
