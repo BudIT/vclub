a1ea335b8b0aadf33e35febed55dd238382f9f67
Yanis Urbis <yanis.urbis@gmail.com>
2016-10-26 14:34:35 +0300

    add resolve prop to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path        = require('path');
 const webpack     = require('webpack');
-const autoreset   = require('postcss-autoreset');
-const normalize   = require('postcss-normalize');
+const autoreset   = require('postcss-autoreset')
+const normalize   = require('postcss-normalize')
 
 
 const DEV = process.env.NODE_ENV !== 'production';
@@ -35,19 +35,16 @@ module.exports = [
           test: /\.css/,
           exclude: /node_modules/,
           loaders: ['style', 'css?modules&importLoaders=1', 'postcss-loader'],
-        },
-        {
-          test: /.(png|gif|jpe?g)(\?[a-z0-9=\.]+)?$/,
-          loader: 'url',
-          query: {
-            limit: 10240,
-            name: '[name]-[hash:6].[ext]',
-          },
         }
       ],
     },
     postcss: function() {
       return [autoreset, normalize]
+    },
+    resolve: {
+      alias: {
+        vclub: path.resolve(__dirname, 'src'),
+      },
     },
     plugins: [
       new webpack.DefinePlugin(Object.assign({}, GLOBALS, { __CLIENT__: true })),
@@ -78,6 +75,11 @@ module.exports = [
           loader: 'babel-loader',
         },
       ],
+    },
+    resolve: {
+      alias: {
+        vclub: path.resolve(__dirname, 'src'),
+      },
     },
     plugins: [
       new webpack.DefinePlugin(Object.assign({}, GLOBALS, { __SERVER__: true })),
