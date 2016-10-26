2f9830874cc7c1fb792b6db0c5775dec357c960c
Yanis Urbis <yanis.urbis@gmail.com>
2016-10-26 15:11:33 +0300

    add img resolver

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,15 @@ module.exports = [
           test: /\.css/,
           exclude: /node_modules/,
           loaders: ['style', 'css?modules&importLoaders=1', 'postcss-loader'],
-        }
+        },
+        {
+          test: /.(png|gif|jpe?g)(\?[a-z0-9=\.]+)?$/,
+          loader: 'url',
+          query: {
+            limit: 10240,
+            name: '[name]-[hash:6].[ext]',
+          },
+        } 
       ],
     },
     postcss: function() {
