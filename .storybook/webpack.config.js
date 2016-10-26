2955f34e074b9cf9b0917c7b6b5fdd0499dcb861
Denis Nevmerzhitskii <kastigarster@gmail.com>
2016-10-26 12:21:39 +0300

    Fix storybook's webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,8 +4,17 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.css$/,
-        loaders: ['style', 'css?modules&importLoaders=1'],
+        test: /\.css/,
+        exclude: /node_modules/,
+        loaders: ['style', 'css?modules&importLoaders=1', 'postcss-loader'],
+      },
+      {
+        test: /.(png|gif|jpe?g)(\?[a-z0-9=\.]+)?$/,
+        loader: 'url',
+        query: {
+          limit: 10240,
+          name: '[name]-[hash:6].[ext]',
+        },
       },
     ],
   },
