5798e3b31251a83f4c82071984ad9974bcd5d5bf
Yanis <yanis.urbis@gmail.com>
2016-10-27 12:06:21 +0300

    add tests for HeaderTab

--- src/views/header/HeaderLeft/HeaderTab/__tests__/HeaderTab.js
+++ src/views/header/HeaderLeft/HeaderTab/__tests__/HeaderTab.js
@@ -1,4 +1,25 @@
-/* eslint-disable */
-test('super simple test', () => {
-  expect(true).toBe(true);
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint-env jest */
+// jest.mock('react/lib/ReactDefaultInjection')
+
+import { changeRoom } from 'vclub/redux/club/rooms';
+
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import HeaderTab from '../HeaderTab';
+
+const tabName = 'VIDEO';
+
+test('HeaderTab calls passed function when HeaderTab is clicked', () => {
+  const dispatchSpy = jest.fn();
+  const wrapper = mount(
+    <HeaderTab isCurrentTab dispatch={dispatchSpy}>
+      {tabName}
+    </HeaderTab>
+  );
+
+  const button = wrapper.find('button');
+  button.simulate('click');
+  expect(dispatchSpy).toHaveBeenCalledTimes(1);
+  expect(dispatchSpy).toHaveBeenCalledWith(changeRoom(tabName));
 });
---
+++ src/views/header/HeaderLeft/HeaderTab/__tests__/HeaderTab.snap.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HeaderTab from '../HeaderTab';
+
+const tabName = 'VIDEO';
+
+test('<HeaderTab /> (current tab) renders correctly', () => {
+  const dispatchSpy = jest.fn();
+  const rendered = renderer.create(
+    <HeaderTab
+      isCurrentTab
+      dispatch={dispatchSpy}
+    >
+      {tabName}
+    </HeaderTab>
+  );
+  expect(rendered.toJSON()).toMatchSnapshot();
+});
+
+test('<HeaderTab /> (not current tab) renders correctly', () => {
+  const dispatchSpy = jest.fn();
+  const rendered = renderer.create(
+    <HeaderTab
+      isCurrentTab={false}
+      dispatch={dispatchSpy}
+    >
+      {tabName}
+    </HeaderTab>
+  );
+  expect(rendered.toJSON()).toMatchSnapshot();
+});
