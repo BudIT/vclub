fa2f954eabb4cdeda9173b11ba36933ae4da603d
Yanis <yanis.urbis@gmail.com>
2016-10-27 12:03:11 +0300

    add test for rooms duck

---
+++ src/redux/club/__tests__/rooms.js
@@ -0,0 +1,36 @@
+import { CHANGE_ROOM, changeRoom } from '../rooms'
+import reducer from '../rooms'
+import initialState from 'vclub/redux/initialClubState'
+
+// action creator
+test('changeRoom action creates proper action', () => {
+  const nextRoom = 'CHAT'
+  const expectedAction = {
+    type: CHANGE_ROOM,
+    payload: nextRoom,
+  }
+  expect(changeRoom(nextRoom)).toEqual(expectedAction)
+})
+
+// reducer
+test('reducer returns initial state', () => {
+  const { rooms: roomsInitialState } = initialState
+  expect(reducer(undefined, {}))
+    .toEqual(roomsInitialState)
+})
+
+test('reducer should handle CHANGE_ROOM', () => {
+  const { rooms: roomsInitialState } = initialState
+
+  let nextRoom = 'CHAT'
+  if (roomsInitialState.currentRoom === 'CHAT') {
+    nextRoom = 'VIDEO'
+  }
+
+  const expectedState = {
+    ...roomsInitialState,
+    currentRoom: nextRoom,
+  }
+
+  expect(reducer(roomsInitialState, changeRoom(nextRoom))).toEqual(expectedState)
+})
