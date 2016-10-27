63c7e9c3d58b881af9303fa1079b22eac0b0b972
Yanis <yanis.urbis@gmail.com>
2016-10-27 22:20:53 +0300

    add fix to tests for auth duck

--- src/redux/club/__tests__/auth.js
+++ src/redux/club/__tests__/auth.js
@@ -9,23 +9,39 @@ import initialState from 'vclub/redux/initialClubState'
 // action creator
 test('logOut action creator creates proper action', () => {
   const nextRoom = 'CHAT'
-  const expectedAction = {
-    type: LOG_OUT,
-    meta: {
-      sideEffect: () => {}
-    },
-  }
   const action = logOut()
-  expect(action.type).toEqual(expectedAction.type)
+
+  expect(action.type).toEqual(LOG_OUT)
+  expect(action.meta.sideEffect).toBeDefined()
+})
+
+test('logOut sideEffect works correctly', () => {
+  const action = logOut()
 
   const ioSocket = {
     emit: jest.fn(),
   };
-
   action.meta.sideEffect({ ioSocket })
 
   expect(ioSocket.emit).toHaveBeenCalledWith('logOut')
   expect(ioSocket.emit).toHaveBeenCalledTimes(1)
+})
+
+test('reducer with logOut works correctly', () => {
+  // after logOut fired state must become initial
+  const state = {
+    ...initialState.auth,
+    authenticated: true,
+    user: {
+      id: "1",
+      name: "Yanis",
+      master: false,
+    }
+  }
+
+  const action = logOut()
+
+  expect(reducer(state, action)).toEqual(initialState.auth)
 })
 
 // reducer
