2e217f453bf2ece48271e661434a692e1a376109
Vira Leskiv <vira777lv@gmail.com>
2016-11-28 19:00:15 +0200

    modified file clubReducer.js, add sharingRoomReducer

--- src/redux/clubReducer.js
+++ src/redux/clubReducer.js
@@ -8,6 +8,7 @@ import authReducer from './club/auth';
 import membersReducer from './club/members';
 import roomsReducer from './club/rooms';
 import uiReducer from './club/ui';
+import sharingRoomReducer from './club/sharing';
 
 
 const baseReducer = combineReducers({
@@ -16,6 +17,7 @@ const baseReducer = combineReducers({
   rooms: roomsReducer,
   ui: uiReducer,
   form: formReducer,
+  sharingRoom: sharingRoomReducer,
 });
 
 export default initHOReducer(baseReducer);
