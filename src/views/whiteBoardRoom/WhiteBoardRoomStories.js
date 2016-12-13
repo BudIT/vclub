a3b2a4142501d092cdf3a97e637ad3b7e9791f92
Yanis <yanis.urbis@gmail.com>
2016-12-13 23:20:24 +0300

    add story for WhiteBoard

---
+++ src/views/whiteBoardRoom/WhiteBoardRoomStories.js
@@ -0,0 +1,20 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+
+import initialState from 'vclub/redux/initialClubState';
+
+import WhiteBoardRoom from './WhiteBoardRoom';
+
+const book = storiesOf('views.whiteBoardRoom', module);
+
+// book.setInitialState({
+//   ...initialState,
+//   whiteboard: {
+//     figures: [1, 1, 1],
+//   },
+// });
+//
+// book.addReduxStory('default view without any figures', (dispatch) => (
+//   <WhiteBoardRoom dispatch={dispatch} />
+// ));
